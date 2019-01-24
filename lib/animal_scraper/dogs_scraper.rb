2950178438a4b365bbca97b3a68c0f2c35aa745a
Fatama Rahman <fatamarahman@gmail.com>
2019-01-24 07:24:54 +0000

    fixed gender and breed

--- lib/animal_scraper/dogs_scraper.rb
+++ lib/animal_scraper/dogs_scraper.rb
@@ -10,9 +10,9 @@ class Dogs_scraper
       dogs_name = dogs_doc.search("div.box-info h4").first.text.strip
       dogs_age = dogs_doc.search("div.box-info span").first.text.strip
       dogs_url = dogs_doc.search('a[href]').first.attributes['href'].value
-      dogs_gender = self.scrape_content(dogs_url)
-      dogs_breed = self.scrape_content(dogs_url)
-      dogs_description = self.scrape_content(dogs_url)
+      dogs_gender = self.scrape_content_gender(dogs_url)
+      dogs_breed = self.scrape_content_breed(dogs_url)
+      dogs_description = self.scrape_content_description(dogs_url)
 
       dogs = Dogs.new(dogs_name, dogs_age, dogs_url, dogs_gender, dogs_breed, dogs_description)
       dogs   
@@ -21,14 +21,24 @@ class Dogs_scraper
 
 
   #scraping for content 
-  def self.scrape_content(dogs_url)
+  def self.scrape_content_gender(dogs_url)
     full_url = URI.join('', dogs_url).to_s
     doc = Nokogiri::HTML(open(full_url))
     dogs_gender = doc.search('div.content-primary span')[1].text.strip
-    dogs_breed = doc.search('div.content-primary span')[2].text.strip
-    dogs_description = doc.search("div.pet-description p").inner_text
     dogs_gender
+  end
+  
+  def self.scrape_content_breed(dogs_url)
+    full_url = URI.join('', dogs_url).to_s
+    doc = Nokogiri::HTML(open(full_url))
+    dogs_breed = doc.search('div.content-primary span')[2].text.strip
     dogs_breed
+  end
+  
+  def self.scrape_content_description (dogs_url)
+    full_url = URI.join('', dogs_url).to_s
+    doc = Nokogiri::HTML(open(full_url))
+    dogs_description = doc.search("div.pet-description p").inner_text
     dogs_description  
   end
 
